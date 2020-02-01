75d9439f53b9d9a7d1ba3b6269a96a0efdecec62
Paul Davies <paulmdavies@gmail.com>
2020-02-01 13:44:55 +0000

    Add possibilities to Square enum

--- src/main/java/com/paulmdavies/traintrackssolver/Square.java
+++ src/main/java/com/paulmdavies/traintrackssolver/Square.java
@@ -1,4 +1,20 @@
 package com.paulmdavies.traintrackssolver;
 
 public enum Square {
+  TRACK_HORIZONTAL(0, "—"),
+  TRACK_VERTICAL(1, "|"),
+  TRACK_LEFT_UP(2, "⌋"),
+  TRACK_RIGHT_UP(3, "⌊"),
+  TRACK_LEFT_DOWN(4, "⌉"),
+  TRACK_RIGHT_DOWN(5, "⌈"),
+  MUST_BE_FILLED(10, "?"),
+  MUST_BE_EMPTY(20, "X");
+
+  public final Integer code;
+  public final String name;
+
+  Square(Integer code, String shortName) {
+    this.code = code;
+    this.name = shortName;
+  }
 }
